211e23094aee432985fcbd39e9a183b49f91db78
Daniel Farnand <dfarnand@collegeboard.org>
2018-02-27 16:52:24 -0500

    Shapefile at efele.net is no longer maintained, updated to new one on github (which also works more robustly), adding code to grab the latest release.

--- inst/update_tz_shapes.R
+++ inst/update_tz_shapes.R
@@ -1,6 +1,12 @@
 library(maptools)
 
-url = "http://efele.net/maps/tz/world/tz_world_mp.zip"
+library(httr)
+library(jsonlite)
+
+api_data <- GET("https://api.github.com/repos/evansiroky/timezone-boundary-builder/releases/latest")
+latest <- fromJSON(content(api_data, "text"))
+
+url <- with(latest$assets, browser_download_url[name == "timezones.shapefile.zip"])
 
 d = tempdir()
 out_file = file.path(d,"world.zip")
@@ -8,6 +14,6 @@ out_file = file.path(d,"world.zip")
 download.file(url, destfile=out_file)
 unzip(out_file, exdir=d)
 
-world_tz_shapes = readShapeSpatial(file.path(d,"world","tz_world_mp.shp"))
+world_tz_shapes = readShapeSpatial(file.path(d,"dist","combined_shapefile.shp"))
 
 save(world_tz_shapes, file = "./world_tz_shapes.rda")
